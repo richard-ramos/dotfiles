17097336c6b60ae3d969b3ff122ee0d9b42e334d
Richard Ramos <info@richardramos.me>
2019-12-25 00:38:41 -0400

    Update neovim.sh

--- scripts/neovim.sh
+++ scripts/neovim.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-sudo apt remove neovim -y
-sudo rm -Rf /usr/bin/nvim
 
-wget --quiet https://github.com/neovim/neovim/releases/latest/download/nvim.appimage --output-document nvim
-
-chmod +x nvim
-sudo chown root:root nvim
-
-sudo mv nvim /usr/bin
+pip3 install neovim
