3bdcaf230782b0727120748a15a6cffc281df251
Richard Ramos <info@richardramos.me>
2019-12-25 00:21:04 -0400

    Update docker.sh

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -10,7 +10,7 @@ sudo apt-get install -y \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
+   disco \
    stable"
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io
