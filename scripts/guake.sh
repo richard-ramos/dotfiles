586774a54b657cd8cca2a4dec4902d1d77713e2a
Richard Ramos <info@richardramos.me>
2019-12-25 00:24:45 -0400

    Update guake.sh

--- scripts/guake.sh
+++ scripts/guake.sh
@@ -3,7 +3,7 @@
 git clone https://github.com/Guake/guake.git guake-repo
 cd guake-repo
 
-$ ./scripts/bootstrap-dev-debian.sh run make
+./scripts/bootstrap-dev-debian.sh run make
 
 make
 sudo make install
